8c50e0a4d0bdb8ee22c236ff3c7f2ad7406a8167
dheerajk <543dheerajkumar@gmail.com>
2022-07-06 22:13:23 +0530

    swagger added(@enableWebMvc is must)

--- src/main/java/com/dynamodb/emp/EmpApplication.java
+++ src/main/java/com/dynamodb/emp/EmpApplication.java
@@ -2,7 +2,9 @@ package com.dynamodb.emp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class EmpApplication {
 
